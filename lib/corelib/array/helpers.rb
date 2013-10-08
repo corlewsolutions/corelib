4ef9ae6b46a3cfbd5c1703a8ec55b386d0f0b7be
Roberts <roberts@corlewsolutions.com>
2013-10-08 16:44:26 -0400

    Add time/date options helpers

--- lib/corelib/array/helpers.rb
+++ lib/corelib/array/helpers.rb
@@ -2,16 +2,64 @@ class Array
 
   def options_label_for(value)
     sub = self.find {|each| each[1] == value}
-    return nil if sub.nil?
-    sub[0]
+    sub.nil? ? nil : sub[0]
+  end
+
+  def self.time_clock_type_options(options={})
+    exemplify = options.fetch(:exemplify, true)
+    id = options.fetch(:start_id, 1)
+    return [["12-Hour Clock", id], ["24-Hour Clock", id+1]] if !exemplify
+
+    t = Time.now
+    s = options.fetch(:separater, " - ")
+    h = options.fetch(:day, t.strftime("%l"))
+    m = options.fetch(:month, t.strftime("%m"))
+    ap =options.fetch(:am_pm, t.strftime("%P"))
+    hm = options.fetch(:day24, t.strftime("%H"))
+    [["12-Hour Clock#{s}(#{h}:#{m}#{ap})", id], ["24-Hour Clock#{s}(#{hm}:#{m})", id+1]]
+  end
+
+  def self.date_format_options(options={})
+    exemplify = options.fetch(:exemplify, true)
+    exemplify ? date_format_options_exemplified_raw(options) : date_format_options_raw(options)
+  end
+
+  def self.date_format_options_raw(options={})
+    id = options.fetch(:start_id, 1)
+    [["MM/DD/YYYY", id], ["MM-DD-YYYY", id+1], ["DD/MM/YYYY", id+2], ["DD-MM-YYYY", id+3], ["YYYY/MM/DD", id+4], ["YYYY-MM-DD", id+5], ["DD.MM.YYYY", id+6], ["YYYY.MM.DD", id+7]]
+  end
+
+  def self.date_format_options_exemplified_raw(options={})
+    id = options.fetch(:start_id, 1)
+    t = Time.now
+    s = options.fetch(:separater, " - ")
+    d = options.fetch(:day, t.strftime("%d"))
+    m = options.fetch(:month, t.strftime("%m"))
+    y = options.fetch(:year, t.year)
+    [["MM/DD/YYYY#{s}(#{m}/#{d}/#{y})", id], ["MM-DD-YYYY#{s}(#{m}-#{d}-#{y})", id+1], ["DD/MM/YYYY#{s}(#{d}/#{m}/#{y})", id+2], ["DD-MM-YYYY#{s}(#{d}-#{m}-#{y})", id+3],
+     ["YYYY/MM/DD#{s}(#{y}/#{m}/#{d})", id+4], ["YYYY-MM-DD#{s}(#{y}-#{m}-#{d})", id+5], ["DD.MM.YYYY#{s}(#{d}.#{m}.#{y})", id+6], ["YYYY.MM.DD#{s}(#{y}.#{m}.#{d})", id+7]]
+  end
+
+  def self.month_options(options={})
+    abbrev = options.fetch(:abbrev, false)
+    list = abbrev ? month_options_abbrev_raw : month_options_raw
+    list.format_options_list(options)
+  end
+
+  def self.month_options_raw(start=1)
+    [["January", start], ["February", start + 1], ["March", start + 2], ["April", start + 3], ["May", start + 4], ["June",  start + 5],
+     ["July", start + 6], ["August", start + 7], ["September", start + 8], ["October", start + 9], ["November", start + 10], ["December", start + 11]]
+  end
+
+  def self.month_options_abbrev_raw(start=1)
+    [["Jan", start], ["Feb", start + 1], ["Mar", start + 2], ["Apr", start + 3], ["May", start + 4], ["Jun", start + 5],
+     ["Jul", start + 6], ["Aug", start + 7], ["Sep", start + 8], ["Oct", start + 9], ["Nov", start + 10], ["Dec", start + 11]]
   end
 
   def self.us_state_options(options={})
     abbrev = options.fetch(:abbrev, false)
-    options[:abbrev] = abbrev #In case abbrev is not in options, set it to the default so sub routines can access
-
     list =  abbrev ? us_state_options_abbrev_raw : us_state_options_raw
-    list.format_raw_us_state_list(options)
+    list.format_options_list(options)
   end
 
   def self.us_state_options_raw
@@ -38,24 +86,24 @@ class Array
      ['WV', 'WV'], ['WI', 'WI'], ['WY', 'WY'],]
   end
 
-  def format_raw_us_state_list(options={})
-    add_blank = options.fetch(:blank, "true")
-    blank_value = options.fetch(:blank_value, 0)
-    blank_label = options.fetch(:blank_label, "")
-    blank_position = options.fetch(:blank_position, 0)
-    self.insert(blank_position, [blank_label, blank_value]) if add_blank
-    change_us_state_list_label_case(options)
+  def format_options_list(options={})
+    insert_options_label_blank(options)
+    change_options_label_case(options) if options[:format] #Change case if :format is set, otherwise use default case
     self
   end
 
 private
 
-  def change_us_state_list_label_case(options)
-    abbrev = options.fetch(:abbrev)
-    label_format = abbrev ? options.fetch(:format, "Up") : options.fetch(:format, "Cap")
-    return self if (label_format == "Up") and abbrev
-    return self if (label_format == "Cap") and !abbrev
+  def insert_options_label_blank(options)
+    add_blank = options.fetch(:blank, "true")
+    blank_value = options.fetch(:blank_value, 0)
+    blank_label = options.fetch(:blank_label, "")
+    blank_position = options.fetch(:blank_position, 0)
+    self.insert(blank_position, [blank_label, blank_value]) if add_blank
+  end
 
+  def change_options_label_case(options)
+    label_format = options.fetch(:format)
     self.each {|opt| opt[0].upcase!} if label_format == "Up"
     self.each {|opt| opt[0].capitalize!} if label_format == "Cap"
     self.each {|opt| opt[0].downcase!} if label_format == "Down"
--- spec/array/helpers_spec.rb
+++ spec/array/helpers_spec.rb
@@ -20,6 +20,25 @@ describe Array do
     end
   end
 
+  describe "#month_options" do
+    it "has the right number of entries" do
+      Array.month_options.size.should == 13
+      Array.month_options(:blank => false).size.should == 12
+    end
+    it "returns the correct list" do
+      Array.month_options[1][0].should == "January"
+      Array.month_options(:abbrev => true)[1][0].should == "Jan"
+    end
+    it "formats correctly" do
+      Array.month_options(:format => "Up")[1][0].should == "JANUARY"
+      Array.month_options(:format => "Down")[1][0].should == "january"
+      Array.month_options(:format => "Cap")[1][0].should == "January"
+      Array.month_options(:format => "Up", :abbrev => true)[1][0].should == "JAN"
+      Array.month_options(:format => "Down", :abbrev => true)[1][0].should == "jan"
+      Array.month_options(:format => "Cap", :abbrev => true)[1][0].should == "Jan"
+    end
+  end
+
   describe "#options_label_for" do
     it "returns the correct label" do
       Array.us_state_options.options_label_for("AK").should == "Alaska"
