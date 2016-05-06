e81c8858eed7b6e4801356f438a51ccc77c1982d
roberts1000 <roberts@corlewsolutions.com>
2016-05-06 19:28:16 -0400

    Update core_spec.rb

--- spec/string/core_spec.rb
+++ spec/string/core_spec.rb
@@ -19,45 +19,4 @@ describe String do
       "".combine("", :if_empty => "-").should == "-"
     end
   end
-
-  describe "#concat_with" do
-    it 'appends correctly' do
-      "".concat_with("test").should == "test"
-      "".concat_with("test",",").should == "test"
-      "my".concat_with("test").should == "mytest"
-      "my".concat_with("test",",").should == "my,test"
-    end
-    it 'does not append separators with nil or empty strings' do
-      "my".concat_with("",",").should == "my"
-      "my".concat_with(nil,",").should == "my"
-      "".concat_with("",",").should == ""
-      "".concat_with(nil,",").should == ""
-    end
-    it 'does not create a new string' do
-      str = ""
-      str_id = str.object_id
-      new_str = str.concat("test")
-      new_str.object_id.should == str_id
-    end
-  end
-
-  describe "#index_of_next_space_from" do
-    it 'calculates correctly with no trailing whitespace' do
-      str = "This is string"
-      str.index_of_next_space_from(0).should == 4
-      str.index_of_next_space_from(5).should == 7
-      str.index_of_next_space_from(8).should == 14
-    end
-    it 'calculates correctly with trailing whitespace' do
-      str = "This is string  "
-      str.index_of_next_space_from(10).should == 14
-      str.index_of_next_space_from(14).should == 15
-    end
-  end
-  describe "#not_empty?" do
-    it "works" do
-      "".not_empty?.should == false
-      "test".not_empty?.should == true
-    end
-  end
 end
