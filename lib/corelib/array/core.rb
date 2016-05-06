861644a4922bddd97d134278c0279e8e1c3e14ef
roberts1000 <roberts@corlewsolutions.com>
2016-05-06 18:55:36 -0400

    Update core.rb

--- lib/corelib/array/core.rb
+++ lib/corelib/array/core.rb
@@ -1,25 +1,11 @@
 class Array
 
-  def to_yes_no(options={})
-    self.collect {|e| e.to_yes_no(options)}
-  end
-
   def add_blank_option(options={})
     doit = options.fetch(:doit, true)
     value = options.fetch(:value, 0)
     label = options.fetch(:label, "")
     self.insert(0, [label, value]) if doit
     self
-  end
-
-  #This method iterates over the Array as normal #each method.  For each iteration
-  #set two variables in the block, |item, flag|. item will be set tot he current item
-  #in the iteration; flag will be set to "false" on all iterations except the last iteration
-  def each_with_end_flag
-    my_size = self.size
-    self.each_with_index do |item, index|
-      index + 1 == my_size ? yield(item, true) : yield(item, false)
-    end
   end
 
   def add_all(arr, options={})
