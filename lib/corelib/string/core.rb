cc4e3beadc5785be7d1eee85564dd4e630d6766a
roberts1000 <roberts@corlewsolutions.com>
2016-05-06 19:17:34 -0400

    Update core.rb

--- lib/corelib/string/core.rb
+++ lib/corelib/string/core.rb
@@ -26,31 +26,6 @@ class String
     self.concat(str)
   end
 
-  #Returns the subset of a string from [0, position] if string[position] is a space.
-  #If string[max] is not a space, it is assumed we are in the middle of a word.
-  #and the logic will increase position a little bit to not break in the middle of a word.
-  def excerpt_to_end_of_word(position=nil)
-     return self if position.nil? or position >= self.size
-
-     char = self[position]
-     return self[0, position].rstrip if char == " "
-
-     self[0,index_of_next_space_from(position)].rstrip
-  end
-
-  #Given a position, return the position of the next space
-  def index_of_next_space_from(position)
-    return nil if self.empty? or position.nil?
-    return nil if position >= self.size
-
-    idx = position
-    (self.size - position).times do
-      idx = idx + 1
-      return idx if self[idx] == " "
-    end
-    idx
-  end
-
 protected
 
     def priv_combine(str, options={})
