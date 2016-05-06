3966255a1b897c127a20ed69d02dcfd39ad27687
roberts1000 <roberts@corlewsolutions.com>
2016-05-06 17:41:17 -0400

    Delete true.rb

--- lib/corelib/boolean/true.rb
+++
@@ -1,9 +0,0 @@
-class TrueClass
-
-	#format accepts (C-Capitalized, U-Uppercase, L-Lowercase)
-	def to_yes_no(options={})
-    value = options.fetch(:if_yes, "Yes")
-    FalseClass.format_to_yes_no(value, options)
-  end
-
-end
